50bf20930b8a9d0eb3259d2aad00560b11bcd1af
Kradeyes <yanin.r.a@mail.ru>
2021-03-31 18:12:11 +0300

    1.2. Режим ожидания.[231217#488030] #2

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,15 +1,11 @@
 package ru.job4j.concurrent;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Wget {
-    public static void main(String[] args) throws InterruptedException {
-        AtomicInteger index = new AtomicInteger();
+    public static void main(String[] args) {
         Thread thread = new Thread(
                 () -> {
                     try {
-                        for (int i = 0; i < 101; i++) {
-                           index.set(i);
+                        for (int index = 0; index < 101; index++) {
                            System.out.print("\rLoading : " + index  + "%");
                            Thread.sleep(1000);
                         }
@@ -19,8 +15,5 @@ public class Wget {
                 }
                 );
         thread.start();
-        thread.join();
-        System.out.println();
-        System.out.println("Loading is complete");
     }
 }
