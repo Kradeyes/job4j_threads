e13c6bf3e4e0ce1aafc44ab1d135b20afa57f56a
Kradeyes <yanin.r.a@mail.ru>
2021-04-02 13:39:15 +0300

    1.1. Состояние нити.[229175#488029] #2

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,17 +1,26 @@
 package ru.job4j.concurrent;
 
+
 public class ThreadState {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
+        System.out.println((first.getName() + " " + first.getState()));
+        System.out.println((second.getName() + " " + first.getState()));
         first.start();
         second.start();
-        first.join();
-        second.join();
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println((first.getName() + " " + first.getState()));
+            System.out.println((second.getName() + " " + first.getState()));
+        }
+        System.out.println((first.getName() + " " + first.getState()));
+        System.out.println((second.getName() + " " + first.getState()));
+
         System.out.println("Work done");
     }
 }
