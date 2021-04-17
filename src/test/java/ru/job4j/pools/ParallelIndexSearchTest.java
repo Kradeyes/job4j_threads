6eaa45708d53eb2810269e1a8f606b0d37dc474a
Kradeyes <yanin.r.a@mail.ru>
2021-04-17 17:35:03 +0300

    rebuild

--- src/test/java/ru/job4j/pools/ParallelIndexSearchTest.java
+++ src/test/java/ru/job4j/pools/ParallelIndexSearchTest.java
@@ -6,17 +6,17 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ParallelIndexSearchTest {
-     private static final Integer[] arrayForTests = {1, 2, 2, 2, 5, 6};
+     private static final Integer[] ARRAY = {1, 2, 2, 2, 5, 6};
 
     @Test
     public void whenFound() {
-      int result = new ParallelIndexSearch<Integer>().findIndex(arrayForTests, 5);
+      int result = new ParallelIndexSearch<Integer>().findIndex(ARRAY, 5);
       assertThat(result, is(4));
   }
 
     @Test
     public void whenNotFound() {
-        int result = new ParallelIndexSearch<Integer>().findIndex(arrayForTests, 8);
+        int result = new ParallelIndexSearch<Integer>().findIndex(ARRAY, 8);
         assertThat(result, is(-1));
   }
 }
